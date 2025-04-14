702e3c67fcc82333892e63d333e23048620a0d95
Teerapat Wassavanich <teerapat.was@techflow.asia>
2025-04-14 23:23:31 +0700

    update lazyvim keymaps config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-local key = vim.keymap
+local map = LazyVim.safe_keymap_set
 local opts = { noremap = true, silent = true, nowait = true }
 
 local function show_relative_path()
@@ -21,46 +21,46 @@ end
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ override default                                        │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>|", "", opts) -- disable Vertical split
-key.set("n", "<leader>-", "", opts) -- disable Horizontal split
-key.set("n", ";", ":", { noremap = true, nowait = true })
-key.set("n", "q", "", opts) -- disable record macro
+map("n", "<leader>|", "", opts) -- disable Vertical split
+map("n", "<leader>-", "", opts) -- disable Horizontal split
+map("n", ";", ":", { noremap = true, nowait = true })
+map("n", "q", "", opts) -- disable record macro
 
-key.set("n", "<leader>$", function()
+map("n", "<leader>$", function()
   return show_relative_path()
 end, { desc = "Show Relative Path" })
-key.set("n", "<leader>%", function()
+map("n", "<leader>%", function()
   return show_project_path()
 end, { desc = "Show Relative th" })
-key.set("n", "Dw", "vb_d", opts) -- delete word backward
-key.set("n", "<C-a>", "gg<S-v>G", opts) -- select all
-key.set("n", "mk", "<cmd>m-2<cr>", opts) -- move line up
-key.set("n", "mj", "<cmd>m+1<cr>", opts) -- move line down
-key.set("n", "+", "<cmd>vertical resize +3<cr>", opts)
-key.set("n", "_", "<cmd>vertical resize -3<cr>", opts)
-key.set("n", ")", "<cmd>horizontal resize +3<cr>", opts)
-key.set("n", "(", "<cmd>horizontal resize -3<cr>", opts)
-key.set("n", "x", '"_x', opts)
-key.set("n", "c", '"_c', opts)
-key.set("v", "c", '"_c', opts)
-key.set("n", "X", '"_X', opts)
-key.set("n", "C", '"_C', opts)
-key.set("v", "C", '"_C', opts)
-key.set("i", "jk", "<ESC>", opts) -- Quick exit insert mode
-key.set("n", "<leader>v", "<cmd>:vsplit<cr>", { desc = "Vertical Split" })
-key.set("n", "<leader>V", "<cmd>:split<cr>", { desc = "Horizontal Split" })
+map("n", "Dw", "vb_d", opts) -- delete word backward
+map("n", "<C-a>", "gg<S-v>G", opts) -- select all
+map("n", "mk", "<cmd>m-2<cr>", opts) -- move line up
+map("n", "mj", "<cmd>m+1<cr>", opts) -- move line down
+map("n", "+", "<cmd>vertical resize +3<cr>", opts)
+map("n", "_", "<cmd>vertical resize -3<cr>", opts)
+map("n", ")", "<cmd>horizontal resize +3<cr>", opts)
+map("n", "(", "<cmd>horizontal resize -3<cr>", opts)
+map("n", "x", '"_x', opts)
+map("n", "c", '"_c', opts)
+map("v", "c", '"_c', opts)
+map("n", "X", '"_X', opts)
+map("n", "C", '"_C', opts)
+map("v", "C", '"_C', opts)
+map("i", "jk", "<ESC>", opts) -- Quick exit insert mode
+map("n", "<leader>v", "<cmd>:vsplit<cr>", { desc = "Vertical Split" })
+map("n", "<leader>V", "<cmd>:split<cr>", { desc = "Horizontal Split" })
 
-key.set("n", "<leader>qh", function()
+map("n", "<leader>qh", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
-key.set("n", "<leader>qH", function()
+map("n", "<leader>qH", function()
   Snacks.bufdelete.other()
 end, { desc = "Delete Other Buffers" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Snacks                                                  │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>'", function()
+map("n", "<leader>'", function()
   Snacks.picker.pick("files", {
     hidden = true, -- Include hidden files (dotfiles)
     no_ignore = true, -- Include files ignored by .gitignore
@@ -71,82 +71,86 @@ end, { desc = "Find files (including hidden)" })
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Avante                                                  │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>a", "", { desc = "Avante" })
+map("n", "<leader>a", "", { desc = "Avante" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Recent                                                  │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>fo", LazyVim.pick("oldfiles"), { desc = "Recent Files" })
-key.set("n", "<leader>fO", function()
+map("n", "<leader>fo", LazyVim.pick("oldfiles"), { desc = "Recent Files" })
+map("n", "<leader>fO", function()
   Snacks.picker.recent({ filter = { cwd = true } })
 end, { desc = "Recent Files (cwd)" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ increment/decrement numbers                             │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>+", "<M-a>", { desc = "Increment number" }) -- increment
-key.set("n", "<leader>-", "<M-x>", { desc = "Decrement number" }) -- decrement
+map("n", "<leader>+", "<M-a>", { desc = "Increment number" }) -- increment
+map("n", "<leader>-", "<M-x>", { desc = "Decrement number" }) -- decrement
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Diagnostics                                             │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "gl", function()
+map("n", "gl", function()
   vim.diagnostic.open_float()
 end, { desc = "Show diagnostics hover" })
 
-key.set("n", "gj", function()
+map("n", "gj", function()
   vim.diagnostic.goto_next()
 end, { desc = "Go to next diagnostic" })
 
-key.set("n", "gk", function()
+map("n", "gk", function()
   vim.diagnostic.goto_prev()
 end, { desc = "Go to previous diagnostic" })
 
-key.set("n", "<leader>\\", "<cmd>LazyExtras<CR>", { desc = "LazyExtras" })
+map("n", "<leader>\\", "<cmd>LazyExtras<CR>", { desc = "LazyExtras" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ LspLens (link JetBrain that show implementatino and     │
 -- │ reference)                                              │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>cL", "<cmd>LspLensToggle<CR>", { desc = "LspLensToggle" })
+map("n", "<leader>cL", "<cmd>LspLensToggle<CR>", { desc = "LspLensToggle" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Terminal                                                │
 -- ╰─────────────────────────────────────────────────────────╯
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+map("n", "<c-/>", function() Snacks.terminal(nil, { cwd = LazyVim.root(), win = { height = 8 } }) end, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", function() Snacks.terminal(nil, { cwd = LazyVim.root(), win = { height = 8 } }) end, { desc = "which_key_ignore" })
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
-key.set("n", "<leader>fL", function()
+map("n", "<leader>fL", function()
   Snacks.terminal(nil, { win = { position = "left" } })
 end, { desc = "Terminal left" })
 
-key.set("n", "<leader>fR", function()
+map("n", "<leader>fR", function()
   Snacks.terminal(nil, { win = { position = "right" } })
 end, { desc = "Terminal right" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Formatting                                              │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set({ "n", "v" }, "gm", function()
+map({ "n", "v" }, "gm", function()
   LazyVim.format({ force = true })
 end, { desc = "Format" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Neotree                                                 │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>e", function()
+map("n", "<leader>e", function()
   require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
 end, { desc = "Explorer NeoTree (cwd)" })
-key.set("n", "<leader>E", function()
+map("n", "<leader>E", function()
   require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
 end, { desc = "Explorer NeoTree (Root Dir)" })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Diffview                                                │
 -- ╰─────────────────────────────────────────────────────────╯
-key.set("n", "<leader>gO", "<cmd>DiffviewOpen<cr>", { noremap = true, silent = true })
-key.set("n", "<leader>gC", "<cmd>DiffviewClose<cr>", { noremap = true, silent = true })
-key.set("n", "<leader>gF", "<cmd>DiffviewFocusFiles<cr>", { noremap = true, silent = true })
-key.set("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", { noremap = true, silent = true })
+map("n", "<leader>gO", "<cmd>DiffviewOpen<cr>", { noremap = true, silent = true })
+map("n", "<leader>gC", "<cmd>DiffviewClose<cr>", { noremap = true, silent = true })
+map("n", "<leader>gF", "<cmd>DiffviewFocusFiles<cr>", { noremap = true, silent = true })
+map("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", { noremap = true, silent = true })
 
 -- ╭─────────────────────────────────────────────────────────╮
 -- │ Code Fold keymap                                        │
