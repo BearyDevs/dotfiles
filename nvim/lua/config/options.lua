db72f4fbc6ba1bba3bcbab7cb57c1ee68386a9a2
Teerapat Wassavanich <teerapat.was@techflow.asia>
2025-05-20 09:02:47 +0700

    update .zshrc

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -40,7 +40,7 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 -- vim.opt.mouse = ""
-vim.opt.mouse = "a" -- Enable mouse scroll and mouse drag
+vim.opt.mouse = "a" -- Enable mouse all mode
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
